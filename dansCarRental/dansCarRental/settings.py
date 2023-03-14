0a7251065b68c7da6514cfd6d3212151ca049baf
Caleb Jeppson <caleb.jeppson@gmail.com>
2023-03-14 11:11:39 -0600

    added basic password reset

--- dansCarRental/dansCarRental/settings.py
+++ dansCarRental/dansCarRental/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"

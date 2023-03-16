56858f342cbd05ad3a7ec485e06947641491f1e8
Caleb Jeppson <caleb.jeppson@gmail.com>
2023-03-16 11:40:28 -0600

    moved password reset pages to get it working

--- dansCarRental/dansCarRental/settings.py
+++ dansCarRental/dansCarRental/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'dansCarRental.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('accounts.templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,10 +127,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "/carRental/static"),
 ]
 
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"

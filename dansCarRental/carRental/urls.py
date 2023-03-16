42571e359111216451c0f1340fec3ef67e879e64
gitjackjones <a02210852@usu.edu>
2023-03-15 21:04:07 -0600

    Created and started working on account page and added it to urls.py

--- dansCarRental/carRental/urls.py
+++ dansCarRental/carRental/urls.py
@@ -4,4 +4,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='carRental/home.html'), name='home'),
+    path('account', TemplateView.as_view(template_name='carRental/account.html'), name='account')
 ]

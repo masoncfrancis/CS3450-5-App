a563f3498e002290a32188913a70730e6b4d86c4
gitjackjones <a02210852@usu.edu>
2023-03-25 18:06:13 -0600

    Added updating user fields feature on account page (add funds field left blank still causes error)

--- dansCarRental/carRental/views.py
+++ dansCarRental/carRental/views.py
@@ -4,6 +4,7 @@ from carRental.models import Vehicle, Reservation, Customer
 from datetime import date
 from decimal import *
 import pprint
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -43,11 +44,29 @@ def car_page(request):
 
 def account_page(request):
     #pprint.pprint(f"\n*** POST dictionary: {request.POST}\m")
-    new_funds = Decimal(request.POST.get('funds', 0))
 
+    # Updating User Info
+    new_first_name = str(request.POST.get('new_first_name', ""))
+    new_last_name = str(request.POST.get('new_last_name', ""))
+    new_email = str(request.POST.get('new_email', ""))
+
+    user = User.objects.get(username=request.user.username)
+
+    if new_first_name != "":
+        user.first_name = new_first_name
+        user.save()
+    if new_last_name != "":
+        user.last_name = new_last_name
+        user.save()
+    if new_email != "":
+        user.email = new_email
+        user.save()
+
+    # Updating Customer Balance
+    new_funds = Decimal(request.POST.get('funds', 0))
     customer_list = Customer.objects.filter(user__username=request.user)
     customer = customer_list[0]
     customer.addMoney(new_funds)
 
-    context = {'balance_foo': customer.balance}
+    context = {'balance': customer.balance}
     return render(request, 'carRental/account.html', context)

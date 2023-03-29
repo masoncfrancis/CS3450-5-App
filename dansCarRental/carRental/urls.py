27d5a39afd3409f06a16cb2eed964a2242792f6e
Caleb Jeppson <caleb.jeppson@gmail.com>
2023-03-29 13:58:45 -0600

    removed redundant urls from branch merging

--- dansCarRental/carRental/urls.py
+++ dansCarRental/carRental/urls.py
@@ -3,8 +3,8 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('account', views.account_page, name="account"),
+    # path('', views.home, name='home'),
+    # path('account', views.account_page, name="account"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('cars', views.car_page, name="cars"),
     path('', TemplateView.as_view(template_name='carRental/home.html'), name='home'),
